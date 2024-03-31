9de10af1c2bbf7439a46cbeca02cec790bd964fc
Alfred Khanyile <mr.khanyile21@gmail.com>
2024-03-31 12:40:03 +0000

    designed a simple registration ui without any responsiveness of components.

--- app/src/main/java/com/example/signupandloginui/MainActivity.java
+++ app/src/main/java/com/example/signupandloginui/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.signupandloginui.login.SignInActivity;
+import com.example.signupandloginui.register.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginButton = findViewById(R.id.loginButton);
+        Button registerButton = findViewById(R.id.register);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/signupandloginui/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.signupandloginui.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.signupandloginui.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
